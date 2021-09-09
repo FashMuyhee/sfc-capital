392b587fd9c12c86feb76630324110320633e64a
FashMuyhee <fashmuyidavid@gmail.com>
2021-09-09 22:05:34 +0100

    google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Fira Sans'],
+        'mulish': ['Mulish'],
+      }
+    },
     colors: {
       yellow: '#FFD873',
       blue: '#152144',
       green: '#10C06D',
       white: "#fff",
-      gray: "#E3E6EA"
+      gray: "#E3E6EA",
+      'gray-2': "#B3B3B3",
     },
   },
   variants: {
