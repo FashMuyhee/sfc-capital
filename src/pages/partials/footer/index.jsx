0bb88931f713d458ef79563100329ff1ece089f0
FashMuyhee <fashmuyidavid@gmail.com>
2021-09-09 23:34:13 +0100

    footer wip

--- src/pages/partials/footer/index.jsx
+++ src/pages/partials/footer/index.jsx
@@ -1,49 +1,43 @@
 import "./style.css";
-import bar from "../../../assets/images/bar.png";
-import leaf from "../../../assets/images/leaf.png";
-import sun from "../../../assets/images/sun.png";
+import apple from "../../../assets/images/apple.png";
+import android from "../../../assets/images/android.png";
+import world from "../../../assets/images/world.png";
+import downloadLine from "../../../assets/images/download-line.png";
 
-const Box = ({ img, title, subtitle }) => (
-  <div className="w-full  box justify-evenly	flex flex-col items-center">
-    <div className="box-icon shadow rounded items-center justify-center flex">
-      <img src={img} alt={title} />
-    </div>
-    <div>
-      <h3 className="text-center text-blue" style={{ fontSize: 25 }}>
-        {title}
-      </h3>
-    </div>
-    <div>
-      <p className="text-center text-gray-2" style={{ fontSize: 14 }}>
-        {subtitle}
-      </p>
+const Box = ({ img, title, download }) => (
+  <div className="w-full md:w-4/12  box justify-evenly flex flex-col items-center bg-green">
+    <img src={img} alt={title} />
+    <div className="justify-around flex flex-row items-center">
+      {download ? <img src={download} alt='"download' /> : <div />}
+      <p>{title}</p>
     </div>
   </div>
 );
 const FooterSection = () => {
   return (
     <footer className="footer-section  md:mt-52  mt-28 flex flex-col items-center justify-center bg-blue">
-      <div>
-        <p className="text-center footer-text">
-          Your guide to financial wellness
-        </p>
+      <div className="footer-title grid grid-cols-1 md:grid-cols-2 md:gap-x-14 gap-y-14 mt-48">
+        <h2>Our channels</h2>
+        <p>Get access to sfs life using any of the following plaforms</p>
       </div>
-      <div className="container reason-wrapper grid grid-cols-1 md:grid-cols-3 md:gap-x-14 gap-y-14 mt-36">
-        <Box
-          title="Invest for growth"
-          subtitle="Invest with us and see your wealth grow"
-          img={bar}
-        />
-        <Box
-          title="Think of the rainy day"
-          subtitle="Get ready for the rainy day. Invest today in our growth instrument."
-          img={sun}
-        />
-        <Box
-          title="Grow your knowledge"
-          subtitle="Learn how to get more from your money with easy-to-understand articles and videos from financial experts."
-          img={leaf}
-        />
+      <div className="container reason-wrapper flex flex-row items-center justify-between mt-10 mb-16 md:gap-x-16 sm:flex-wrap md:flex-no-wrap	">
+        {/* <div className="flex flex-row items-center justify-between bg-green download-link md:w-2/3"> */}
+        <div className="md:w-2/3 flex flex-row items-center justify-between md:flex-no-wrap flex-wrap">
+          <Box
+            img={android}
+            download={downloadLine}
+            title="Download on Play Store"
+          />
+          <Box
+            img={apple}
+            download={downloadLine}
+            title="Download on App Store"
+          />
+          <Box img={world} title="Get Started Online" />
+        </div>
+        <div className="bg-green md:w-1/4">
+          <Box img={apple} />
+        </div>
       </div>
     </footer>
   );
